e95f87372e8f05444fdae20ba9e9896f830aca5d
byhyakimaro <moderrevolution090217@gmail.com>
2021-11-19 17:23:52 -0300

    save game

--- public/Game.js
+++ public/Game.js
@@ -28,7 +28,7 @@ class Game {
   playerSprites = []
   playerReverse = false
   playerSpritesIndex = 0
-  playerAction = 'Walking'
+  playerAction = 'Idle'
   playerWidth = 128
   playerHeight = 128
   gravity = 1.4
@@ -51,6 +51,14 @@ class Game {
     })
   }
 
+  setAction(actionSet) {
+    if(!this.playerSprites.find(({ action }) => action === actionSet)) {
+      console.log(this.playerSprites)
+      throw new Error('Invalid action')
+    }
+    this.playerAction = actionSet
+  }
+
   drawText(Text, textX, textY) {
     ctx.save()
     ctx.fillStyle = '#000000'
@@ -70,9 +78,9 @@ class Game {
     ctx.restore()
   }
 
-  drawObj(ObjX, ObjY, ObjWidth, ObjHeight) {
+  drawObj(Color, ObjX, ObjY, ObjWidth, ObjHeight) {
     ctx.save()
-    ctx.fillStyle = '#000000'
+    ctx.fillStyle = Color
     ctx.fillRect(ObjX, ObjY, ObjWidth, ObjHeight)
     ctx.restore()
 
@@ -90,36 +98,42 @@ class Game {
     let nowTime = Date.now()
     this.fps = Math.round(1000 / (nowTime - this.oldTime))
     this.oldTime = nowTime
-    this.drawText(`x: ${Math.round(this.playerX)} y: ${Math.round(this.playerY)} fps: ${this.fps}`, 10, 30)
-  
+    this.drawText(`velX:${Math.round(this.velX)} velY:${Math.round(this.velY)} x: ${Math.round(this.playerX)} y: ${Math.round(this.playerY)} fps: ${this.fps}`, 10, 30)
+    
+    if(!Object.keys(pressed).length) this.setAction('Idle')
+
     const floor = canvas.height - this.playerHeight
     if (this.playerY > floor) {
       this.playerY = floor
       this.velY = 0
-
+      
       if(pressed.w) {
         this.velY =- this.jumpForce
-        this.playerAction = 'Jump Loop'
-        setTimeout(()=>this.playerAction = 'Walking',500) 
+        this.setAction('Jump Loop')
+        setTimeout(()=>this.setAction('Walking'), 500)
       }
     }
-
+    
     if(pressed.a) {
       this.velX -= this.speed
       this.playerReverse = true
+      this.setAction('Walking')
     }
     if(pressed.d) {
       this.velX += this.speed
       this.playerReverse = false
+      this.setAction('Walking')
     }
     if(pressed.s) this.velY += this.speed
-  
+    
+    this.drawObj('#000000',canvas.width/1.8, canvas.height-200, 100, 10)
+    
     this.velX *= this.friction
     this.velY += this.gravity
   
     this.playerX += this.velX
     this.playerY += this.velY
-
+    
     this.playerSpritesIndex ++
     const spritesLength = this.playerSprites.find(({ action }) => action === this.playerAction)
     if(this.playerSpritesIndex >= spritesLength.sprites) this.playerSpritesIndex = 0
@@ -128,7 +142,9 @@ class Game {
     playerImg.src = `images/SpritesPlayer/Reaper_Man_${this.playerSkin}/${this.playerAction}/0_Reaper_Man_Walking_${this.playerSpritesIndex}.png`
     this.drawPlayer(playerImg, this.playerX, this.playerY, this.playerWidth, this.playerHeight)
     
-    this.drawObj(canvas.width/1.8, canvas.height-200, 100, 10)
+    ctx.strokeStyle = 'red'
+    ctx.strokeRect(this.playerX, this.playerY, this.playerWidth, this.playerHeight)
+
     setTimeout(()=>this.animatePlayer(),1000/this.framesDelay)
   }
 }
