6afa112463dad0b1a2c285a590e13f3f7598ad90
byhyakimaro <moderrevolution090217@gmail.com>
2021-11-25 16:49:03 -0300

    save

--- socket.ts
+++ socket.ts
@@ -7,13 +7,14 @@ const io = new Server(httpServer)
 const connected = {}
 
 io.on('connection',(socket)=>{
+  console.log(`${socket.id} connected`)
   socket.on("disconnect", () => {
     console.log(`${socket.id} disconnected`)
   })
 
   socket.on('register', (nick)=>{
     const data = setRegister(nick)
-    connected[data.token] = data
+    connected[data.token] = { nick: data.nick, hex: data.hex, token: data.token, socket }
     socket.emit('newRegister', Buffer.from(JSON.stringify(data)).toString('base64'))
   })
 
