1717ba6b5e087f586d390d154e9185c9ea35c489
byhyakimaro <moderrevolution090217@gmail.com>
2021-11-29 11:23:45 -0300

    save

--- public/Engine.js
+++ public/Engine.js
@@ -103,3 +103,16 @@ document.addEventListener('click', (event) => {
     }
   }
 })
+
+document.addEventListener('keypress', ({ key }) => {
+  const message = document.querySelector('#text-msg')
+  if(key==='Enter') {
+    if(message.value) {
+      const msgs = document.createElement('div')
+      msgs.setAttribute('id','message')
+      msgs.innerHTML = `<b style="color:${document.game.data.hex}">${document.game.data.nick}: </b>${message.value}`
+      document.querySelector('.messages').appendChild(msgs)
+      message.value = ''
+    }
+  }
+})
