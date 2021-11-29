a76151a2f17fdf468612957ec316d811a68fd638
byhyakimaro <moderrevolution090217@gmail.com>
2021-11-29 12:43:54 -0300

    save

--- public/Map.js
+++ public/Map.js
@@ -1,16 +1,15 @@
 export default class Map {
-
-  constructor(canvas, ctx) {
+  constructor (canvas, ctx) {
     this.canvas = canvas
     this.ctx = ctx
 
     this.drawObj('black', 0, 0, canvas.width, canvas.height)
-    for(const [ x, y, w, h ] of document.game.blocks) {
-      this.drawObj('white',x,y,w,h)
+    for (const [x, y, w, h] of document.game.blocks) {
+      this.drawObj('white', x, y, w, h)
     }
   }
 
-  drawObj(Color, ObjX, ObjY, ObjWidth, ObjHeight) {
+  drawObj (Color, ObjX, ObjY, ObjWidth, ObjHeight) {
     this.ctx.save()
     this.ctx.fillStyle = Color
     this.ctx.fillRect(ObjX, ObjY, ObjWidth, ObjHeight)
